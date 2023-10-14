45962f0509979fca290938eca95e3bb76fff0497
MARAT <almat59@rambler.ru>
2023-10-14 21:00:06 +0300

    add func title

--- str_func.py
+++ str_func.py
@@ -1,6 +1,15 @@
 def upper_sentence(sentence):
+    """Функция возвращает все буквы предложения заглавными."""
     return sentence.upper()
 
 sentence = "Hello world"
 
 print(upper_sentence(sentence))
+
+def title_sentence(sentence):
+    """Функция возвращает первые буквы слов заглавными."""
+    return sentence.title()
+
+sentence = "hello world"
+
+print(title_sentence(sentence))
